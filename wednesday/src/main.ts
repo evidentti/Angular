42b565f4f3b306e08288deb008467551a209ca13
Hannu Honka <hannu.honka@tieto.com>
2017-01-17 09:08:24 +0200

    enable prod mode

--- wednesday/src/main.ts
+++ wednesday/src/main.ts
@@ -1,4 +1,8 @@
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {AppModule} from './app/app.module';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+// uncomment rows below to use in prod mode
+// import { enableProdMode } from '@angular/core';
+// enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(AppModule);
