cfaa96522ff47586448622090110d6d275e83db4
Hannu Honka <hannu.honka@tieto.com>
2017-01-17 08:22:04 +0200

    wednesday

--- tuesday/src/app/app.module.ts
+++ tuesday/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
-import {HttpModule, JsonpModule} from '@angular/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
-import {AppComponent} from './app.component';
-import {routing, appRoutingProviders} from './app.routing';
-import {HomeComponent} from './home/home.component';
-import {AboutComponent} from './about/about.component';
-import {MaterialComponent} from './material/material.component';
+import { AppComponent } from './app.component';
+import { routing, appRoutingProviders } from './app.routing';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MaterialComponent } from './material/material.component';
 
 @NgModule({
     declarations: [
@@ -21,6 +22,7 @@ import {MaterialComponent} from './material/material.component';
         FormsModule,
         HttpModule,
         JsonpModule,
+        MaterialModule.forRoot(),
         routing
     ],
     providers: [appRoutingProviders],
---
+++ wednesday/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {HttpModule, JsonpModule} from '@angular/http';
+import { MaterialModule } from '@angular/material';
+
+import {AppComponent} from './app.component';
+import {routing, appRoutingProviders} from './app.routing';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        AboutComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        JsonpModule,
+        MaterialModule.forRoot(),
+        routing
+    ],
+    providers: [appRoutingProviders],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule {
+}
