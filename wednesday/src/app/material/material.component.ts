348aeacfee18934168a3bbda929d5f4c72df120b
Hannu Honka <hannu.honka@tieto.com>
2017-01-18 04:25:11 +0200

    “backup”

--- wednesday/src/app/material/material.component.ts
+++ wednesday/src/app/material/material.component.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
 })
 
 export class MaterialComponent {
+    private text: string;
+
+    constructor() {
+        console.log('MaterialComponent CONSTRUCTOR');
+    }
+
+    openDialog(par: string) {
+        this.text = par;
+        console.debug(par, '->', this.text);
+    }
 }
