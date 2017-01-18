70e01ef4827f1dbc7fa17b87a064ce89ae5ad0f9
Hannu Honka <hannu.honka@tieto.com>
2017-01-18 05:01:19 +0200

    “backup”

--- wednesday/src/app/material/material.component.ts
+++ wednesday/src/app/material/material.component.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef } from '@angular/core';
+import { DialogsService } from '../dialog/dialogs.service';
 
 @Component({
     selector: 'material',
-    template: require('./material.component.html')
+    template: require('./material.component.html'),
+    providers: [DialogsService]
 })
 
 export class MaterialComponent {
     private text: string;
+    public result: any;
 
-    constructor() {
+    constructor(private dialogsService: DialogsService, private viewContainerRef: ViewContainerRef) {
         console.log('MaterialComponent CONSTRUCTOR');
     }
 
-    openDialog(par: string) {
+    clicked(par: string) {
         this.text = par;
         console.debug(par, '->', this.text);
+    }
+
+    openDialog() {
+        console.debug('open dialog');
+        this.dialogsService
+            .confirm('Confirm Dialog', 'Are you sure you want to do this?', this.viewContainerRef)
+            .subscribe(res => this.result = res);
     }
 }
--- wednesday/src/main.ts
+++ wednesday/src/main.ts
@@ -1,5 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { MaterialModule } from '@angular/material';
+import { DialogsModule } from './app/dialog/dialogs.module';
 
 // uncomment rows below to use in prod mode
 // import { enableProdMode } from '@angular/core';
